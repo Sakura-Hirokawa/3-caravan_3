de3e5b5aa5a0cc57765b22e139a351415636adea
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-07 01:31:56 +0000

    [Finish]3-3/カラム名間違いのため変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_210922) do
+ActiveRecord::Schema.define(version: 2021_04_07_012029) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "category"
-    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "body"
   end
 
 end
